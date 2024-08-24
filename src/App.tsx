df46150fd74f427fec71ebbcbda1c36e8b6d3055
brovnie <bronka1.dev@gmail.com>
2024-08-24 19:44:55 +0200

    Layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { Sidebar } from './components/Sidebar';
 
 function App() {
   return (
-    <div className="grid grid-cols-5 grid-rows-5 gap-4 h-screen">
+    <div className="flex gap-4 h-screen">
       <Sidebar />
-      <div className="col-span-4 row-span-5"></div>
+      <div className="border border-slate-500 w-full"></div>
     </div>
   );
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,16 +2,28 @@ import React from 'react';
 
 export const Sidebar = () => {
   return (
-    <div className="row-span-5 border border-red-500 min-w-52">
-      <div className="flex flex-row gap-6 items-center justify-center px-5 py-3">
-        <p className="text-xl">Add table</p>
-        <input
-          type="number"
-          min="0"
-          max="10"
-          placeholder="0"
-          className="w-10 text-center border border-slate-500 rounded"
-        />
+    <div className="border border-red-500 min-w-52">
+      <div className="flex flex-col gap-6 items-center justify-center px-5 py-3">
+        <p className="text-xl">Table</p>
+        <form className="flex flex-col gap-4">
+          <div className="flex flex-row gap-4">
+            <label htmlFor="seats">Seats</label>
+            <input
+              name="seats"
+              type="number"
+              min="0"
+              max="10"
+              placeholder="0"
+              className="w-10 text-center border border-slate-500 rounded"
+            />
+          </div>
+          <button
+            type="submit"
+            className="border border-green-500 bg-green-400 text-white px-3 py-2 rounded"
+          >
+            Add table
+          </button>
+        </form>
       </div>
     </div>
   );
